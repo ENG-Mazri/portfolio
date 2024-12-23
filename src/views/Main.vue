08f0845c919deaab6c773d970c1d7aef8de64384
Abderrahmane <amazri@tangl.cloud>
2024-12-23 21:30:53 +0100

    add text highlights

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -3,7 +3,12 @@ Layout
     .flex.flex-wrap.justify-evenly.items-center.gap-10.select-none.duration-1500( v-animateonscroll="{ enterClass: 'fadein', leaveClass: 'fadeout' }")
         .flex.flex-col.max-w-200
             h1.drop-shadow-2xl.m-0 Mazri Abderrahmane
-            p.max-w-100.px-10 I'm a highly motivated software developer with a passion for creating elegant and user-friendly solutions. I enjoy collaborating with cross-functional teams to achieve project goals.
+            //- p.max-w-100.px-10 I'm a highly motivated software developer with a passion for creating elegant and user-friendly solutions. I enjoy collaborating with cross-functional teams to achieve project goals.
+            n-highlight.max-w-100.px-10(
+                :text="text"
+                :patterns="patterns"
+                :highlight-style="{ color: '#646e68', background: 'none', 'font-weight': 'bold'}"
+            )
 
         img.max-w-70.rounded-full(class="img" src="../assets/img.png")
 
@@ -18,7 +23,8 @@ export default defineComponent({
     components: { Layout },
     data(){
         return{
-            
+            text: "I'm a highly motivated software developer with a passion for creating elegant and user-friendly solutions. I enjoy collaborating with cross-functional teams to achieve project goals.",
+            patterns: ['motivated', 'elegant', 'user-friendly', 'collaborating', 'goals'],
         }
     },
     mounted(){
