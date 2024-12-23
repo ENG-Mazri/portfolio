2d722cbc86226cbd4fce92a463065acc7046dfaa
Abderrahmane <amazri@tangl.cloud>
2024-12-23 23:38:52 +0100

    fix styling for small screens

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -4,13 +4,14 @@ Layout
         .flex.flex-col.max-w-200
             h1.drop-shadow-2xl.m-0 Mazri Abderrahmane
             //- p.max-w-100.px-10 I'm a highly motivated software developer with a passion for creating elegant and user-friendly solutions. I enjoy collaborating with cross-functional teams to achieve project goals.
-            n-highlight.max-w-100.px-8(
+            n-highlight.max-w-100.px-5(
+                class="sm:px-8"
                 :text="text"
                 :patterns="patterns"
                 :highlight-style="{ color: '#646e68', background: 'none', 'font-weight': 'bold'}"
             )
 
-        img.max-w-40.rounded-full(class="img sm:max-w-70" src="../assets/img.png")
+        img.max-w-60.rounded-full(class="img sm:max-w-70" src="../assets/img.png")
 
 
 
